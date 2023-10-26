8c441ef39d0a61803723d39f6cd0a03b45b160af
MatashiHa <maestr0o.mario@yandex.ru>
2023-10-26 20:30:43 +0300

    Cleanup

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,23 +2,16 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 import { initialProfile } from "@/lib/initial-profile";
 
-
 export default async function Home() {
   const profile = await initialProfile();
 
-
-  if (profile){
+  if (profile) {
     return (
-    
-
       <div>
-        <UserButton
-        afterSignOutUrl="/"
-        />
-        <ModeToggle/>
-        <p className="h-full flex text-2xl font-bold"> You've been logged in!</p>
+        <UserButton afterSignOutUrl="/" />
+        <ModeToggle />
+        <p className="h-full flex text-2xl font-bold">You've been logged in!</p>
       </div>
-    )
+    );
   }
-  
 }

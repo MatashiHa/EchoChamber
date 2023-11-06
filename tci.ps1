64881adab79683f363cad8cbb4141d2a26ee9b3b
TotsamyAS <unn-alex@yandex.ru>
2023-11-06 23:02:26 +0300

    script tci.ps1 updated with a description comment

--- tci.ps1
+++ tci.ps1
@@ -1,3 +1,4 @@
+#Этот скрипт устанавливает все нужные пакеты и компоненты. запустите этот скрипт командой .\tci.ps1
 npx shadcn-ui@latest add dialog
 npx shadcn-ui@latest add input
 npx shadcn-ui@latest add form

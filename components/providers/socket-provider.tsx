579ddafbf66c0a97af47ecd8e463bef5e1d2063b
TotsamyAS <unn-alex@yandex.ru>
2023-11-30 23:50:25 +0300

    Bug resolved

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -33,7 +33,8 @@ export const SocketProvider =  ({
 
     useEffect(() => {
         const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
-            path: "/api/socket/io",
+            // эта строка не меняется, там страшные ошибки, если именно её изменить
+            path: "api/socket/io",
             addTrailingSlash: false
         })
         socketInstance.on("connect",() => {

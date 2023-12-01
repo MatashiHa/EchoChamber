fbbb001592a1a6afe75484b81ca3863f15f21ddd
TotsamyAS <unn-alex@yandex.ru>
2023-12-01 19:09:54 +0300

    Message sending fixed

--- app/(main)/(routes)/chambers/[chamberId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/chambers/[chamberId]/channels/[channelId]/page.tsx
@@ -52,7 +52,7 @@ const ChannelIdPage = async ({params}:ChannelIdPageProps) => {
                 chatId={channel.id}
                 type="channel"
                 apiUrl="external/messages"
-                socketUrl="api/socket/messages"
+                socketUrl="/api/socket/messages"
 
                 socketQuery = {{
                     channelId: channel.id,
@@ -63,7 +63,7 @@ const ChannelIdPage = async ({params}:ChannelIdPageProps) => {
                 />
                 <ChatInput
                     name = {channel.name}
-                    apiUrl = "api/socket/messages"                
+                    apiUrl = "/api/socket/messages"                
                     type = "channel"
                     query = {{
                         channelId: channel.id,
